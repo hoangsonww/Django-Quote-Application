7f3afe01059024173f4dd5818f61b11439cdb575
Son Nguyen <hoangson091104@gmail.com>
2024-06-12 10:30:30 +0700

    Final: App Working

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Quote, Like, Comment
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class QuoteTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.quote = Quote.objects.create(text='Test Quote', author='Test Author', category='General')
+
+    def test_quote_creation(self):
+        self.assertEqual(self.quote.text, 'Test Quote')
+        self.assertEqual(self.quote.author, 'Test Author')
+        self.assertEqual(self.quote.category, 'General')
+
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse('index')
+        self.search_url = reverse('search')
+        self.category_url = reverse('category_view', args=['General'])
+
+    def test_index_view(self):
+        response = self.client.get(self.index_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_view(self):
+        response = self.client.get(self.search_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_view(self):
+        response = self.client.get(self.category_url)
+        self.assertEqual(response.status_code, 200)
+
+
+class SearchTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.search_url = reverse('search')
+        Quote.objects.create(text='Test Quote', author='Test Author', category='General')
+
+    def test_search_quote(self):
+        response = self.client.get(self.search_url, {'query': 'Test Quote'})
+        self.assertContains(response, 'Test Quote')
