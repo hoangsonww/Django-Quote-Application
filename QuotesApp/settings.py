dc5704b05dd884e34014193fd5311c0157658475
Son Nguyen <hoangson091104@gmail.com>
2024-06-12 09:54:09 +0700

    Final: App Working

--- QuotesApp/settings.py
+++ QuotesApp/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your_django_secret_key'
+# SECRET_KEY = 'your_django_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
